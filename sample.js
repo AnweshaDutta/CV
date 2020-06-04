e3bd816e354b0f4fb959912541a8a27954ca48ed
AnweshaDutta <32648830+AnweshaDutta@users.noreply.github.com>
2020-06-05 00:07:35 +0530

    Add files via upload
    Initial commit

---
+++ sample.js
@@ -0,0 +1,13 @@
+// function openpage(){
+//    var name = "This is my first java scipt"
+//    document.write(name)
+// }  
+var a = 5
+var b = 6
+function addition(){
+  var x = a+b
+  document.getElementById("demo").innerHTML = x
+  //document.write(a+b)
+ // document.write("demo is " + total)
+ // alert(total)
+}
